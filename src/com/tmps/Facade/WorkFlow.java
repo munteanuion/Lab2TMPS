1194332b932f791c3c4c4bad8be87d956b14dc89
munteanuion <munteanu.ion@isa.utm.md>
2022-04-14 12:59:57 +0300

    final

---
+++ src/com/tmps/Facade/WorkFlow.java
@@ -0,0 +1,14 @@
+package com.tmps.Facade;
+
+public class WorkFlow {
+    Developer developer = new Developer();
+    Job job = new Job();
+    BugTracker bugTracker = new BugTracker();
+
+    public void solveProblems(){
+        job.doJob();
+        bugTracker.startSprint();
+        developer.doJobBeforeDEadLine(bugTracker);
+    }
+
+}
